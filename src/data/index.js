e9cdbfafcc3365d1c3e825af28c8bdb21d0cba33
Zadat Olayinka <bundayy@outlook.com>
2020-03-18 11:39:34 +0100

    works updated

--- src/data/index.js
+++ src/data/index.js
@@ -1,13 +1,86 @@
-
 const Works = [
-    {
-        id: 1,
-        title: 'First Project',
-        description: 'This projects does it all really',
-        image: '/images/think.svg',
-        framework: ['React','Ionic','Laravel']
-    }
+  {
+    id: 1,
+    title: "ChipTranz",
+    description:
+      "ChipTranz is an android mobile app used to save funds, I designed and built this product with simplicity in mind and while giving priority to a smooth UI/UX experience for the users ",
+    image:
+      "https://res.cloudinary.com/bundayy/image/upload/v1584523007/portfolio/Screen_Shot_2020-01-13_at_11.22.58_AM_rntehq.png",
+    tech: ["Ionic", "Tailwind", "Firebase", "Capacitor"],
+    link:
+      "https://play.google.com/store/apps/details?id=com.wavyscript.chiptranz"
+  },
+  {
+    id: 2,
+    title: "Frannies Luxury Shop",
+    description:
+      "Frannies Luxury Shop is an online store which was built to the satisfaction of the owner, I kept the design simple while " +
+      "selling the products to the customer. It is integrated wiith a payment system, an investory system, an order management system and also a delivery " +
+      "management system which manages the whole customers",
+    image:
+      "https://res.cloudinary.com/bundayy/image/upload/v1584523095/portfolio/Screen_Shot_2020-03-18_at_10.18.04_AM_jxh3hl.png",
+    tech: ["Wordpress", "Bootstrap", "Paystack"],
+    link: "https://franniesluxuryshop.com/"
+  },
+  {
+    id: 3,
+    title: "Cyclotron Engineering Service",
+    description:
+      "Cyclotron is an engineering company portfolio website, colors and deisgns were choosen based on the clients request and was delivered within a short period.",
+    image:
+      "https://res.cloudinary.com/bundayy/image/upload/v1584523777/portfolio/cyclon_lkt8tl.png",
+    tech: ["Wordpress", "Bootstrap"],
+    link: "http://cyclotroneng.com/"
+  },
+  {
+    id: 4,
+    title: "Twitter History Fetcher",
+    description:
+      "This is my first personal project developed with React and deployed to Heroku for test purposes",
+    image:
+      "https://res.cloudinary.com/bundayy/image/upload/v1584523123/portfolio/Screen_Shot_2020-03-16_at_12.10.39_PM_gxykif.png",
+    tech: ["React", "Twitter", "Tailwind", "heroku"],
+    link: "https://simple-react-demo.herokuapp.com/twitter"
+  },
+  {
+    id: 5,
+    title: "Aspom Portal",
+    description:
+      "This is a portal I developed with my former employer for a client which deals with travels. It handles all travel papers application process with more than 5 different roles on the app",
+    image:
+      "https://res.cloudinary.com/bundayy/image/upload/v1584523262/portfolio/Screen_Shot_2020-03-18_at_10.20.54_AM_hlz4fo.png",
+    tech: ["Laravel", "Bootstrap"],
+    link: "https://portal.aspom.com.ng"
+  },
+  {
+    id: 6,
+    title: "Diagrammart Design System",
+    description:
+      "I developed a software solution used by an architectural business unit to collect orders of design system from users, get paid for theiir designs while they deliver to customers. Unfortunately this project after being used for a while is currently on hold for personal reasons",
+    image:
+      "https://res.cloudinary.com/bundayy/image/upload/v1584523418/portfolio/Screen_Shot_2020-03-18_at_10.23.25_AM_w9coej.png",
+    tech: ["Laravel", "Bootstrap", "Paystack"],
+    link: ""
+  },
+  {
+    id: 7,
+    title: "Whot APP",
+    description:
+      "I worked with a friend to deliver a real time Whot Playing APP, this app works on both iOS and Android. The app involves playing with online users, while each player bets with an agreed amount. ",
+    image:
+      "https://res.cloudinary.com/bundayy/image/upload/v1584523972/portfolio/whot_tw7csm.png",
+    tech: ["Ionic", "Firebase"],
+    link: ""
+  },
+  {
+    id: 8,
+    title: "Unibadan Post Graduate Recommendation Portal",
+    description:
+      "While in my finals in University of Ibadan in 2017, I worked on a project for the Post Graduate School, I built a system where prospective students are managed by the HOD, Supervisor, Lecturer etc before being assigned to a Supervisor for their Course of Study. It was a project built to scale, considering the number of applications the Univeristy gets annually.",
+    image: "/images/think.svg",
+    tech: ["Laravel","Bootstrap"],
+    link: "http://pgsui.com/pgdev/public"
+  }
 ];
-
 
 export default Works;
