f9191ef17952125cfb84b9094c1e9671eba5525b
Zadat Olayinka <bundayy@outlook.com>
2020-03-18 09:35:51 +0100

    links updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Work from "./components/Work";
 import Works from "./data";
 
 function App() {
-  console.log(Works);
   return (
     <div className="bg-black h-screen text-white flex px-32 overflow-y-auto">
       <div className="w-1/4 py-12 mr-4">
@@ -13,9 +12,41 @@ function App() {
         <div className="text-md text-tint leading-6">
           <p className="py-4">
             I'm a software engineer from Lagos, Nigeria. I do both front-end and
-            back-end development, I've built a couple of mobile apps with Ionic.
-            I do back-end work with mostly Laravel, and I'm currently building
-            front-ends with React.js and Tailwind CSS.
+            back-end development, I've built a couple of mobile apps with {" "}
+            <a
+              href="https://ionicframework.com/"
+              className="text-white "
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Ionic
+            </a>
+            . I do back-end work with mostly {" "}
+            <a
+              href="https://laravel.com/"
+              className="text-white "
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Laravel
+            </a>, and I'm currently building
+            front-ends with {" "}
+            <a
+              href="https://reactjs.org/"
+              className="text-white "
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              React.js
+            </a> and {" "}
+            <a
+              href="https://tailwindcss.com/"
+              className="text-white "
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Tailwind CSS
+            </a>.
           </p>
           <p className="py-4">
             I believe in utility-first CSS and I'm currently adopting test
@@ -23,12 +54,19 @@ function App() {
             (coming up soon).
           </p>
           <p className="py-4">
-            I hangout on Leetcode, HackerRank and Code AppSignal solving
+            I hangout on Leetcode, HackerRank and Code Signal solving
             algorithm problems always trying to optimize speed and save space.
           </p>
           <p className="py-4">
-            I'm available for contracts and remote roles. Want us to
-            work together? You should contact me.
+            I'm available for contracts and remote roles. Want us to work
+            together? You should <a
+              href="mailto:bundayy@outlook.com?subject=Hi 🙂, I have a Project I would like to work with you on"
+              className="text-white "
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              contact me
+            </a>.
           </p>
         </div>
 
