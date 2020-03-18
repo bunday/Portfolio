297b377ce6cea81b1ccfbf0ee32c77bc72063813
Zadat Olayinka <bundayy@outlook.com>
2020-03-18 08:56:01 +0100

    bio updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./index.css";
 import Footer from "./components/Footer";
 import Work from "./components/Work";
-import  Works  from './data';
+import Works from "./data";
 
 function App() {
   console.log(Works);
@@ -10,11 +10,27 @@ function App() {
     <div className="bg-black h-screen text-white flex px-32 overflow-y-auto">
       <div className="w-1/4 py-12 mr-4">
         <p className="text-4xl font-bold">Hello there, I'm Zadat Olayinka</p>
-        <p>
-          {" "}
-          then the short story goes here bro then the short story goes here bro
-          then the short story goes here bro
-        </p>
+        <div className="text-md text-tint leading-6">
+          <p className="py-4">
+            I'm a software engineer from Lagos, Nigeria. I do both front-end and
+            back-end development, I've built a couple of mobile apps with Ionic.
+            I do back-end work with mostly Laravel, and I'm currently building
+            front-ends with React.js and Tailwind CSS.
+          </p>
+          <p className="py-4">
+            I believe in utility-first CSS and I'm currently adopting test
+            driven development work. I plan to share what I learn on my blog
+            (coming up soon).
+          </p>
+          <p className="py-4">
+            I hangout on Leetcode, HackerRank and Code AppSignal solving
+            algorithm problems always trying to optimize speed and save space.
+          </p>
+          <p className="py-4">
+            I'm available for contracts and remote roles. Want us to
+            work together? You should contact me.
+          </p>
+        </div>
 
         {/* <Contact/> */}
 
@@ -29,9 +45,8 @@ function App() {
         </p>
 
         <div className="flex grid-cols-2">
-          <Work/>
+          <Work />
         </div>
-        
       </div>
       <div></div>
     </div>
